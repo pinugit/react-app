10ac508cf580c7e12c5d3beb710e8ccf98c21865
priyanshu <pinu@DESKTOP-D0TLML6>
2023-07-15 12:30:32 +0530

    in LIstGroup.tsx file added a way to conditionalize the return statement

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,6 +1,14 @@
 function ListGroup() {
-  const citis = ["ahmedabad", "bihar", "kankroli", "udaipur"];
-
+  let citis = ["ahmedabad", "bihar", "kankroli", "udaipur"];
+  citis = [];
+  if (citis.length == 0) {
+    return (
+      <>
+        <h1>List</h1>
+        <h2>no items found</h2>
+      </>
+    );
+  }
   return (
     <>
       <h1>List</h1>
