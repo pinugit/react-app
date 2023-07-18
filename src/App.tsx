e68c2225c7f6400df3c0385c404a6f7dbbb3def4
priyanshu <pinu@DESKTOP-D0TLML6>
2023-07-18 17:09:14 +0530

    last commit for the dat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+// import ListGroup from "./Components/ListGroup";
+import Button from "./Components/Button";
 
 function App() {
-  //   let citis = ["kankroli", "ahmedabad", "udaipur"];
+  return (
+    <div>
+      <Button onButtonClick={() => null}>click me</Button>
+    </div>
+  );
+  // let citis = ["kankroli", "ahmedabad", "udaipur"];
+  // return <ListGroup items={citis} heading="citis" onSelectItem={() => null} />;
   //   const [alertVisibility, setAlertVisibility] = useState(false);
   //   const handleButtonClick = (text: string) => {
   //     console.log(text);
--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,15 +1,15 @@
 import { ReactNode, useState } from "react";
+import styled from "./Button.module.css";
 
 interface props {
   children: string;
-  buttonColor?: "primary" | "secondary" | "danger" | "close";
   onButtonClick: () => void;
 }
-function Button({ children, buttonColor = "primary", onButtonClick }: props) {
+function Button({ children, onButtonClick }: props) {
   const [clickedState, setClickedState] = useState(0);
   return (
     <button
-      className={"btn btn-" + buttonColor}
+      className={styled.ButtonStyle}
       onClick={() => {
         onButtonClick();
       }}
