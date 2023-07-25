8e3652ad1196e427c3724f9f9b3f24eb993901ba
pinugit <pinuwork@gmail.com>
2023-07-25 17:04:07 +0530

    made adjustment in the form component

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           itemList.push(dict);
           console.log(dict);
           console.log(itemList);
-          <Table items={itemList} />;
         }}
       />
     </div>
