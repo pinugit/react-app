4cd9dbdde98a55b941ed474622b751ba0fcb55a5
pinugit <pinuwork@gmail.com>
2023-07-27 15:36:47 +0530

    currently learning backendstuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Table from "./Components/Table/Table";
 import TheForm from "./Components/TheForm/TheForm";
-type Item = {
-  description: any;
-  amount: any;
-  category: any;
-};
+import axios from "axios";
+// type Item = {
+//   description: any;
+//   amount: any;
+// category: any;
+// };
+interface data {
+  id: number;
+  name: string;
+}
 function App() {
-  const [listItems, setListItems] = useState<Item[]>([]);
+  const [names, setNames] = useState<data[]>([]);
+  // const [listItems, setListItems] = useState<Item[]>([]);
+  useEffect(() => {
+    axios
+      .get<data[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setNames(res.data));
+    console.log(names);
+  }, []);
+
   return (
-    <div>
-      <TheForm
-        onSutbmitPassDict={(dict) => {
-          setListItems([...listItems, dict]);
-        }}
-      />
-      <Table
-        items={listItems}
-        onClickDelete={(item) => {
-          let updatedItems = [...listItems];
-          updatedItems.splice(updatedItems.indexOf(item), 1);
-          setListItems(updatedItems);
-        }}
-      />
-    </div>
+    <ul>
+      {names.map((name) => (
+        <li key={name.id}>{name.name}</li>
+      ))}
+    </ul>
   );
+
+  // <div>
+  //   <TheForm
+  //     onSubmitPassDict={(dict) => {
+  //       setListItems([...listItems, dict]);
+  //     }}
+  //   />
+  //   <Table
+  //     items={listItems}
+  //     onClickDelete={(item) => {
+  //       let updatedItems = [...listItems];
+  //       updatedItems.splice(updatedItems.indexOf(item), 1);
+  //       setListItems(updatedItems);
+  //     }}
+  //   />
+  // </div>
 }
 export default App;
